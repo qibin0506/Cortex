f77b05b87436b4ece310acf281b6afcda1c49ab9
qibin <qibin0506@users.noreply.github.com>
2025-08-26 17:47:10 +0800

    Update file_dataset.py

--- file_dataset.py
+++ file_dataset.py
@@ -21,7 +21,7 @@ class FileDatasetBase(FileDataset):
         if not os.path.exists(file_path):
             if TrainerTools().parallel.is_main_process:
                 print(f"正在下载{file_path}")
-                _download(self.download_url.format(self.file_names[idx]), file_path)
+                start_download(self.download_url.format(self.file_names[idx]), file_path)
                 # _download(self.download_url.format(self.file_names[idx]), file_path)
 
             TrainerTools().parallel.wait()
@@ -33,7 +33,7 @@ class FileDatasetBase(FileDataset):
             if os.path.exists(dst_file):
                 os.remove(dst_file)
 
-            threading.Thread(target=_download, args=(self.download_url.format(next_file), dst_file)).start()
+            threading.Thread(target=start_download, args=(self.download_url.format(next_file), dst_file)).start()
 
         # 删除前一个文件
         if idx > 0 and TrainerTools().parallel.is_main_process:
