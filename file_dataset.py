627beea67090697ff0ba5393b0135c2a451ab322
Qi Bin <qibin0506@users.noreply.github.com>
2025-09-08 08:57:46 +0000

    使用modelscope下载数据集文件

--- file_dataset.py
+++ file_dataset.py
@@ -2,14 +2,13 @@ import os
 import threading
 from llm_trainer import FileDataset, TrainerTools
 from constant import data_root_dir
-from downloader import simple_download
+from modelscope import dataset_snapshot_download
 
 
 class FileDatasetBase(FileDataset):
 
     def __init__(self, file_names: list):
         self.file_names = file_names
-        self.download_url = 'https://www.modelscope.cn/datasets/qibin0506/cortex-train-data-v2/resolve/master/{}'
 
     def __len__(self) -> int:
         return len(self.file_names)
@@ -21,7 +20,11 @@ class FileDatasetBase(FileDataset):
         if not os.path.exists(file_path):
             if TrainerTools().parallel.is_main_process:
                 print(f"正在下载{file_path}")
-                simple_download(self.download_url.format(self.file_names[idx]), file_path)
+                dataset_snapshot_download(
+                    'qibin0506/cortex-train-data-v2',
+                    allow_file_pattern=[self.file_names[idx]],
+                    local_dir=data_root_dir
+                )
 
             TrainerTools().parallel.wait()
 
@@ -32,7 +35,14 @@ class FileDatasetBase(FileDataset):
             if os.path.exists(dst_file):
                 os.remove(dst_file)
 
-            threading.Thread(target=simple_download, args=(self.download_url.format(next_file), dst_file)).start()
+            threading.Thread(
+                target=dataset_snapshot_download,
+                kwargs={
+                    'dataset_id': 'qibin0506/cortex-train-data-v2',
+                    'allow_file_pattern': [next_file],
+                    'local_dir': data_root_dir
+                }
+            ).start()
 
         # 删除前一个文件
         if idx > 0 and TrainerTools().parallel.is_main_process:
